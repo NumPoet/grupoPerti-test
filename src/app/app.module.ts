6c0b9b102c4c074db168b0896123aba7372af8d3
NumPoet <cbi2143044162@titlani.uam.mx>
2021-09-27 15:13:11 -0500

    Agregando service worker y PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,9 +20,18 @@ import { AppComponent } from './app.component';
     BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule,
-    IonicStorageModule.forRoot()
+    IonicStorageModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    // OneSignal, 
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/push.service.ts
+++ src/app/services/push.service.ts
@@ -7,28 +7,35 @@ import { OneSignal } from '@ionic-native/onesignal/ngx';
 export class PushService {
 
   constructor(
-    // private oneSignal: OneSignal
+    private oneSignal: OneSignal
     ) { }
 
   configuracionInicial() {
-    // //b2f7f966-d8cc-11e4-bed1-df8f05be55ba
-    // this.oneSignal.startInit('f18b8a43-5de5-4798-be29-62bb587f1a7a', '239273328008');
+    //b2f7f966-d8cc-11e4-bed1-df8f05be55ba
+    this.oneSignal.startInit('f18b8a43-5de5-4798-be29-62bb587f1a7a', '239273328008');
 
-    // this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
 
-    // this.oneSignal.handleNotificationReceived().subscribe((notificacion) => {
-    // // do something when notification is received
-    //   console.log('Notificación Recibida', notificacion);
+    this.oneSignal.handleNotificationReceived().subscribe((notificacion) => {
+    // do something when notification is received
+      console.log('Notificación Recibida', notificacion);
       
-    // });
+    });
 
-    // this.oneSignal.handleNotificationOpened().subscribe((notificacion) => {
-    //   // do something when a notification is opened
-    //   console.log('Notificación Abierta', notificacion);
-    // });
+    this.oneSignal.handleNotificationOpened().subscribe((notificacion) => {
+      // do something when a notification is opened
+      console.log('Notificación Abierta', notificacion);
+    });
 
-    // this.oneSignal.endInit();
+    this.oneSignal.endInit();
   
+
+
+    
+
+
+
+
   }
 
 
